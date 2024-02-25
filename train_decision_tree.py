28b63ad775a675526c34c6c1c9ad7b699f98c5f8
greentea <greentea@greenteaui-MacBookPro.local>
2024-02-25 17:50:36 +0900

    WIP - making tree

--- train_decision_tree.py
+++ train_decision_tree.py
@@ -1,11 +1,15 @@
-import ipdb
-import torch
 import pickle
+
+import ipdb
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier as De
+import torch
 from hummingbird.ml import convert
-from data import HeartFailureDataset
 from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier as De
+from sklearn.tree import _tree
+
+from data import HeartFailureDataset
+
 
 # custom collate_fn
 def collate_fn(data):
@@ -24,8 +28,8 @@ if __name__ == "__main__":
     x_train, x_test, y_train, y_test = HeartFailureDataset(split="train").get_data()
 
     """Get Train Configurations"""
-    #todo: fix scikit-learn seed
-    #torch.manual_seed(17)
+    # todo: fix scikit-learn seed
+    # torch.manual_seed(17)
     clr = De()
     clr.fit(x_train, y_train)
 
@@ -45,9 +49,7 @@ if __name__ == "__main__":
     )
 
     txt = tree.export_text(clr)
-    # print(txt)
-    # ipdb.set_trace()
-    from sklearn.tree import _tree
+
     def tree_to_code(tree, feature_names):
         tree_ = tree.tree_
         feature_name = [
@@ -56,27 +58,30 @@ if __name__ == "__main__":
         ]
 
         nodes = []
+
         def recurse(node, depth, idx):
             if tree_.feature[node] != _tree.TREE_UNDEFINED:
                 name = feature_name[node]
                 threshold = tree_.threshold[node]
                 nodes.append([idx, name, threshold])
                 idx = recurse(tree_.children_left[node], depth + 1, idx + 1)
-                nodes.append([idx+1, name, threshold])
+                nodes.append([idx + 1, name, threshold])
                 idx = idx + 1
                 idx = recurse(tree_.children_right[node], depth + 1, idx + 1)
             else:
-                nodes.append([-1, idx, tree_.value[node]])
+                nodes.append([-1, idx, np.argmax(tree_.value[node][0])])
 
             return idx
+
         recurse(0, 1, 0)
+        ipdb.set_trace()
         print(nodes)
+
+
     feature_names = [str(i) for i in range(18)]
     tree_to_code(clr, feature_names)
 
-        
-
     """Checkpointing"""
     PATH = f"./data/decision_tree.pkl"
-    with open(PATH, 'wb') as f:
+    with open(PATH, "wb") as f:
         pickle.dump(clr, f)
